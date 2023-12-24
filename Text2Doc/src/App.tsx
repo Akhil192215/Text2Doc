f374b4e3fdea2e2e9c44471d87c52ae7c6ba110e
akhilspoovathinkal <akhilspoovathinkal@gmail.com>
2023-12-24 18:24:07 +0530

    feat: textEditor setup

--- Text2Doc/src/App.tsx
+++ Text2Doc/src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import TextEditor from './component/Editor'
-
-
+import "./App.css";
+import TextEditor from "./component/Editor";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [doc, setDoc] = useState("");
+
+  useEffect(() => {
+    console.log(doc);
+  }, [doc]);
+
+  const handleChange = (e) => {
+    console.log(e);
+  };
 
   return (
     <>
-      <div>
-       <h3 className='text-red-700'>Hi</h3>
-       <TextEditor/>
+      <div className="space-y-4">
+        <h3 className="text-3xl font-semibold">Create custom document</h3>
+        <TextEditor
+          placeholder="Enter your doc body"
+          onChange={(e: any) => {
+            setDoc(e);
+          }}
+        />
       </div>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
