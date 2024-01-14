e0f27f39b0ef4a2258b576ef42ee8fcacf6cd4b6
akhilspoovathinkal <akhilspoovathinkal@gmail.com>
2024-01-14 19:05:35 +0530

    refactor: generator updated

--- Text2Doc/server/utils/doc_generator.go
+++ Text2Doc/server/utils/doc_generator.go
@@ -1,5 +1,3 @@
-// utils/utils.go
-
 package utils
 
 import (
@@ -10,18 +8,18 @@ import (
 )
 
 func GeneratePDF(docHTML string) {
-	// PDF_DATA := fmt.Sprintf(`
-	// <div style="margin-top:30px">
-	// 	<div style="margin-top:10px">%s</div>
-	// 	<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-	// 		<p style="font-size: 12px; color: #888; background-color: white; padding: 0 10px;">
-	// 			----------- end of document -----------
-	// 		</p>
-	// 	</div>
-	// </div>
-	// `, docHTML)
-	// fmt.Printf(docHTML)
-	PDF_DATA := "<p>Hello, World!</p>"
+	PDF_DATA := fmt.Sprintf(`
+	<div style="margin-top:30px">
+		<div style="margin-top:10px">%s</div>
+		<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+			<p style="font-size: 12px; color: #888; background-color: white; padding: 0 10px;">
+				----------- end of document -----------
+			</p>
+		</div>
+	</div>
+	`, docHTML)
+	fmt.Printf(docHTML)
+	PDF_DATA = "<p>Hello, World!</p>"
 
 	htmlReader := strings.NewReader(PDF_DATA)
 
